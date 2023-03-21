87cff92a9053e14c611c269492cafb950561feff
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-03-21 17:10:39 +0000

    First comment

---
+++ Pokedex.swift
@@ -0,0 +1,50 @@
+// Write your code below 🤾‍♂️
+class Pokemon {
+  var num = 0
+  var name = ""
+  var type = [""]
+  var ability = [""]
+
+  init(num: Int, name: String, type: [String], ability: [String]) {
+  self.num = num
+  self.name = name
+  self.type = type
+  self.ability = ability
+  }
+
+  func displayInfo() {
+    print("No.       #\(num)")
+    print("Name      \(name)")
+    print("Type      \(type)")
+    print("Abilities \(ability)")
+    print("===================================")
+  }
+}
+
+var bullbasaur = Pokemon(num: 1, name: "Bulbasaur", type: ["Grass 🌱", "Poison 💀"], ability: ["Overgrow"])
+
+var charmander = Pokemon(num: 4, name: "Charmander", type: ["Fire 🔥"], ability: ["Blaze"])
+
+var squirtle = Pokemon(num: 7, name: "Squirtle", type: ["Water 💧"], ability: ["Torrent"])
+
+bullbasaur.displayInfo()
+charmander.displayInfo()
+squirtle.displayInfo()
+
+class GigantamaxPokemon: Pokemon {
+  var location = ""
+  init(num: Int, name: String, type: [String], ability: [String], location: String) {
+  self.location = location
+  super.init(num: num, name: name, type: type, ability: ability)
+}
+
+  override func displayInfo() {
+    print("No.       #\(num)")
+    print("Name      \(name)")
+    print("Type      \(type)")
+    print("Abilities \(ability)")
+    print("Location \(location)")
+    print("===================================")
+  }
+}
+
